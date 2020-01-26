719e221859198ff9b5d785775873fe40066871a2
taher_kawantwala <taherkawantwala@gmail.com>
2020-01-26 21:45:54 +0530

    First commit

---
+++ Src/main.c
@@ -0,0 +1,467 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2020 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define mutexSHORT_DELAY     ((uint32_t) 20)
+#define mutexNO_DELAY        ((uint32_t) 0)
+#define mutexTWO_TICK_DELAY  ((uint32_t) 2)
+/* Private variables ---------------------------------------------------------*/
+static osMutexId osMutex;
+
+UART_HandleTypeDef huart1;
+
+/* Variables used to detect and latch errors. */
+static __IO uint32_t HighPriorityThreadCycles = 0, MediumPriorityThreadCycles = 0, LowPriorityThreadCycles = 0;
+
+/* Handles of the two higher priority tasks, required so they can be resumed
+   (unsuspended). */
+static osThreadId osHighPriorityThreadHandle, osMediumPriorityThreadHandle;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MutexHighPriorityThread(void const *argument);
+static void MutexMeduimPriorityThread(void const *argument);
+static void MutexLowPriorityThread(void const *argument);
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+
+  /* Creates the mutex  */
+    osMutexDef(osMutex);
+    osMutex = osMutexCreate(osMutex(osMutex));
+
+    if(osMutex != NULL)
+    {
+      /* Define and create the high priority thread */
+      osThreadDef(MutHigh, MutexHighPriorityThread, osPriorityBelowNormal, 0, configMINIMAL_STACK_SIZE);
+      osHighPriorityThreadHandle = osThreadCreate(osThread(MutHigh), NULL);
+
+      /* Define and create the medium priority thread */
+      osThreadDef(MutMedium, MutexMeduimPriorityThread, osPriorityLow, 0, configMINIMAL_STACK_SIZE);
+      osMediumPriorityThreadHandle = osThreadCreate(osThread(MutMedium), NULL);
+
+      /* Define and create the low priority thread */
+      osThreadDef(MutLow, MutexLowPriorityThread, osPriorityIdle, 0, configMINIMAL_STACK_SIZE);
+      osThreadCreate(osThread(MutLow), NULL);
+    }
+
+    /* Start scheduler */
+    osKernelStart();
+
+    /* We should never get here as control is now taken by the scheduler */
+    for(;;);
+}
+
+static void MutexHighPriorityThread(void const *argument)
+{
+  /* Just to remove compiler warning. */
+  (void) argument;
+
+  for(;;)
+  {
+    /* The first time through the mutex will be immediately available, on
+    subsequent times through the mutex will be held by the low priority thread
+    at this point and this Take will cause the low priority thread to inherit
+    the priority of this tadhr.  In this case the block time must be
+    long enough to ensure the low priority thread will execute again before the
+    block time expires.  If the block time does expire then the error
+    flag will be set here. */
+    if(osMutexWait(osMutex, mutexTWO_TICK_DELAY) != osOK)
+    {
+      /* Toggle LED 3 to indicate error */
+    }
+
+    /* Ensure the other thread attempting to access the mutex
+    are able to execute to ensure they either block (where a block
+    time is specified) or return an error (where no block time is
+    specified) as the mutex is held by this task. */
+    osDelay(mutexSHORT_DELAY);
+
+    /* We should now be able to release the mutex .
+    When the mutex is available again the medium priority thread
+    should be unblocked but not run because it has a lower priority
+    than this thread.  The low priority thread should also not run
+    at this point as it too has a lower priority than this thread. */
+    if(osMutexRelease(osMutex) != osOK)
+    {
+      /* Toggle LED 3 to indicate error */
+    }
+
+    /* Keep count of the number of cycles this thread has performed. */
+    HighPriorityThreadCycles++;
+
+    /* Suspend ourselves to the medium priority thread can execute. */
+    osThreadSuspend(NULL);
+  }
+}
+
+/**
+  * @brief  Mutex Medium Priority Thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+static void MutexMeduimPriorityThread(void const *argument)
+{
+  /* Just to remove compiler warning. */
+  (void) argument;
+
+  for(;;)
+  {
+    /* This thread will run while the high-priority thread is blocked, and the
+    high-priority thread will block only once it has the mutex - therefore
+    this call should block until the high-priority thread has given up the
+    mutex, and not actually execute past this call until the high-priority
+    thread is suspended. */
+    if(osMutexWait(osMutex, osWaitForever) == osOK)
+    {
+      if(osThreadGetState(osHighPriorityThreadHandle) != osThreadSuspended)
+      {
+        /* Did not expect to execute until the high priority thread was
+        suspended.
+        Toggle LED 3 to indicate error */
+      }
+      else
+      {
+        /* Give the mutex back before suspending ourselves to allow
+        the low priority thread to obtain the mutex. */
+        if(osMutexRelease(osMutex) != osOK)
+        {
+          /* Toggle LED 3 to indicate error */
+
+        }
+        osThreadSuspend(NULL);
+      }
+    }
+    else
+    {
+      /* We should not leave the osMutexWait() function
+      until the mutex was obtained.
+      Toggle LED 3 to indicate error */
+
+    }
+
+    /* The High and Medium priority threads should be in lock step. */
+    if(HighPriorityThreadCycles != (MediumPriorityThreadCycles + 1))
+    {
+      /* Toggle LED 3 to indicate error */
+
+    }
+
+    /* Keep count of the number of cycles this task has performed so a
+    stall can be detected. */
+    MediumPriorityThreadCycles++;
+
+  }
+}
+
+/**
+  * @brief  Mutex Low Priority Thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+static void MutexLowPriorityThread(void const *argument)
+{
+  /* Just to remove compiler warning. */
+  (void) argument;
+
+  for(;;)
+  {
+    /* Keep attempting to obtain the mutex.  We should only obtain it when
+    the medium-priority thread has suspended itself, which in turn should only
+    happen when the high-priority thread is also suspended. */
+    if(osMutexWait(osMutex, mutexNO_DELAY) == osOK)
+    {
+      /* Is the haigh and medium-priority threads suspended? */
+      if((osThreadGetState(osHighPriorityThreadHandle) != osThreadSuspended) || (osThreadGetState(osMediumPriorityThreadHandle) != osThreadSuspended))
+      {
+        /* Toggle LED 3 to indicate error */
+
+      }
+      else
+      {
+        /* Keep count of the number of cycles this task has performed
+        so a stall can be detected. */
+        LowPriorityThreadCycles++;
+
+
+        /* We can resume the other tasks here even though they have a
+        higher priority than the this thread. When they execute they
+        will attempt to obtain the mutex but fail because the low-priority
+        thread is still the mutex holder.  this thread will then inherit
+        the higher priority.  The medium-priority thread will block indefinitely
+        when it attempts to obtain the mutex, the high-priority thread will only
+        block for a fixed period and an error will be latched if the
+        high-priority thread has not returned the mutex by the time this
+        fixed period has expired. */
+        osThreadResume(osMediumPriorityThreadHandle);
+        osThreadResume(osHighPriorityThreadHandle);
+
+        /* The other two tasks should now have executed and no longer
+        be suspended. */
+        if((osThreadGetState(osHighPriorityThreadHandle) == osThreadSuspended) || (osThreadGetState(osMediumPriorityThreadHandle) == osThreadSuspended))
+        {
+          /* Toggle LED 3 to indicate error */
+
+        }
+
+        /* Release the mutex, disinheriting the higher priority again. */
+        if(osMutexRelease(osMutex) != osOK)
+        {
+          /* Toggle LED 3 to indicate error */
+
+        }
+      }
+    }
+
+#if configUSE_PREEMPTION == 0
+    {
+      taskYIELD();
+    }
+#endif
+  }
+}
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, BTN_0_Pin|BTN_1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_0_Pin|LED_1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : BTN_0_Pin BTN_1_Pin */
+  GPIO_InitStruct.Pin = BTN_0_Pin|BTN_1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_0_Pin LED_1_Pin */
+  GPIO_InitStruct.Pin = LED_0_Pin|LED_1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM16 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM16) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
